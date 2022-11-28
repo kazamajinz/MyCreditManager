e10a37bdb77d4d69f068cd0096dc70d0b036f1a6
Pane <kazamajinz@icoinnet.co>
2022-11-28 19:23:26 +0900

    221128_Pane: 수정

--- MyCreditManager/Comment.swift
+++ MyCreditManager/Comment.swift
@@ -34,64 +34,44 @@ func viewDefaultComment(_ comment: DefaultComment) {
     }
 }
 
-func viewErrorComment(_ error: ErrorComent) {
+func viewErrorComment(_ error: ErrorComent, _ name: String? = nil) {
     switch error {
     case .서브에러:
         print("입력이 잘못되었습니다. 다시 확인해주세요.")
     case .학생추가에러:
-        return
+        print("\(name ?? "")은 이미 존재하는 학생입니다. 추가하지 않습니다.")
     case .학생삭제에러:
-        return
+        print("\(name ?? "") 학생을 찾지 못했습니다.")
     default:
         print("뭔가 입력이 잘못되었습니다. 1~5 사이의 숫자 혹은 X를 입력해주세요.")
     }
 }
 
-func viewErrorComment(_ error: ErrorComent, _ name: String) {
-    switch error {
-    case .서브에러:
-        return
-    case .학생추가에러:
-        print("\(name)은 이미 존재하는 학생입니다. 추가하지 않습니다.")
-    case .학생삭제에러, .성적삭제에러, .평점보기에러:
-        print("\(name) 학생을 찾지 못했습니다.")
-    default:
-        return
-    }
-}
-
-func viewResultComment(_ result: ResultComment) {
-    if result == .종료 {
+func viewResultComment(_ result: ResultComment, _ name: String? = nil, _ subject: String? = nil, _ score: String? = nil, _ subjectData: [String:String]? = nil) {
+    switch result {
+    case .종료:
         print("프로그램을 종료합니다...")
-    }
-}
-
-func viewResultComment(_ result: ResultComment, _ name: String) {
-    if result == .학생추가 {
-        print("\(name) 학생을 추가했습니다.")
-    } else if result == .학생삭제 {
-        print("\(name) 학생을 삭제하였습니다.")
-    }
-}
-
-func viewResultComment(_ result: ResultComment, _ name: String, _ subject: String, _ score: String) {
-    if result == .성적추가 {
-        print("\(name) 학생의 \(subject) 과목이 \(score)로 추가(변경)되었습니다.")
-    }
-}
-
-func viewResultComment(_ result: ResultComment, _ name: String, _ subject: String) {
-    if result == .성적삭제 {
-        print("\(name) 학생의 \(subject) 과목의 성적이 삭제되었습니다.")
-    }
-}
-    
-func viewResultComment(_ result: ResultComment, _ subjectData: [String:String]) {
-    if result == .평점보기 {
-        subjectData.forEach { subject, score in
+    case .학생추가:
+        guard let value = name else { return }
+        print("\(value) 학생을 추가했습니다.")
+    case .학생삭제:
+        guard let value = name else { return }
+        print("\(value) 학생을 삭제하였습니다.")
+    case .성적추가:
+        guard let value = name else { return }
+        guard let value2 = subject else { return }
+        guard let value3 = score else { return }
+        print("\(value) 학생의 \(value2) 과목이 \(value3)로 추가(변경)되었습니다.")
+    case .성적삭제:
+        guard let value = name else { return }
+        guard let value2 = subject else { return }
+        print("\(value) 학생의 \(value2) 과목의 성적이 삭제되었습니다.")
+    case .평점보기:
+        guard let arr = subjectData else { return }
+        arr.forEach { subject, score in
             print("\(subject): \(score)")
         }
-        let average = subjectData.values.map{scoreDic[$0] ?? 0.0}.reduce(0.0,+)/Double(subjectData.keys.count)
+        let average = arr.values.map{scoreDic[$0] ?? 0.0}.reduce(0.0,+)/Double(arr.keys.count)
         print("평점 : \(trunc(average * 100) / 100)")
     }
 }
